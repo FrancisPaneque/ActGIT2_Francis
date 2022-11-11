c1f4f0e52bee74dd5791ad3e137f4629d8fc2e68
Francis <fran.pnq@gmail.com>
2022-11-11 13:49:19 +0100

    Añadimo programaV1

---
+++ src/ActGIT2.java
@@ -0,0 +1,13 @@
+
+public class ActGIT2 {
+  
+  public static void main(String[] args) {
+    
+    int[] numerosAleatorios = new int[20];
+    
+    for (int i = 0; i < 20; i++) {
+      numerosAleatorios[i] = (int)(Math.random() * (10 - 1 + 1 ) + 1);  
+    }
+  }
+
+}
