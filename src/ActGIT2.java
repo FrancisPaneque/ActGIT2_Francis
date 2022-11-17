5c15035852bd4b0842474d32b9151b60673929a4
Francis <fran.pnq@gmail.com>
2022-11-17 20:13:17 +0100

    cambiamos de 20 a 30

--- src/ActGIT2.java
+++ src/ActGIT2.java
@@ -3,9 +3,9 @@ public class ActGIT2 {
   
   public static void main(String[] args) {
     
-    int[] numerosAleatorios = new int[20];
+    int[] numerosAleatorios = new int[30];
     
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 30; i++) {
       numerosAleatorios[i] = (int)(Math.random() * (10 - 1 + 1 ) + 1);  
     }
   }
